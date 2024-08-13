e30ab0c53daaa49fbc023d019cadb7540ae04b6d
Gourav Rajhans <kbgaurav004@gmail.com>
2024-08-13 16:30:47 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Demo from './Components/Demo'
+import Hero from './Components/Hero'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+       <div className="main">
+            <div className='gradient' />
+        </div> 
+
+        <div className="app">
+            <Hero />
+            <Demo />
+        </div>
+    </main>
+  )
+}
+
+export default App
